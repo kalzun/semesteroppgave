9f65b5e0bba107c5d590c180443873321041d307
Erik And <erikand@gmail.com>
2019-05-03 23:21:38 +0200

    Search suggestions show for all inputfields

--- js/main.js
+++ js/main.js
@@ -451,103 +451,11 @@ var Kommuneobj = function(navn, id) {
 	this.people = new People(id);
 }
 
-// Kjøres når brukeren trykker på en søkeknapp
-function search(){
-	let kommunenr1,
-		kommunenr2;
-    const iD = event.target.id;
-    const domElem = document.getElementsByClassName(iD)[0];
-    const aParent = event.target.parentElement.parentElement;
-    const alleInputs = aParent.querySelectorAll("div .search");
-
-    switch (iD){
-    	case "detaljer":
-    		// Hvis bruker skriver inn kommunenavn:
-			// Convert name to id
-			(isNameInDataset(alleInputs[0].value)) ? kommunenr1 = convertToId(alleInputs[0].value) : kommunenr1 = alleInputs[0].value;
-    		break;
-    	case "sammenligning":
-    		(isNameInDataset(alleInputs[0].value)) ? kommunenr1 = convertToId(alleInputs[0].value) : kommunenr1 = alleInputs[0].value;
-    		(isNameInDataset(alleInputs[1].value)) ? kommunenr2 = convertToId(alleInputs[1].value) : kommunenr2 = alleInputs[1].value;
-    		break;
-    };
-
-    ds.onload = function(){
-    	clearOutput(); // Fjerne regex-forslag
-		removeLoadingMessage()
-        tabell(domElem, iD, kommunenr1, kommunenr2);
-    }
-    if (l.isLoaded() == true) {
-        ds.onload();
-    }else{
-        displayLoadingMessage(domElem)
-    }
-}
-
-
 //Må gjøres penere
 var ds = new DataSet(allUrls);
 ds.load()
 var l = AlleKommunerSingleton.getInstance()
 
-document.addEventListener("DOMContentLoaded", function(event){
-	const searchButtons = document.querySelectorAll(".searchbutton");
-	for (let index = 0; index < 2; index++){
-		searchButtons[index].addEventListener("click", search);
-	}
-
-	// Input-listener som sjekker for hver bokstav skrevet inn
-	inputFields = document.querySelectorAll(".search");
-	for (let i = 0; i < inputFields.length; i++){
-		inputFields[i].addEventListener("input", regexChecker);
-	}
-});
-
-function regexChecker(event){
-	const names = l.getAllNames();
-	const IDs = l.getAllIDs();
-	let userInput = event.target.value;
-	const output = document.querySelector(".search-output");
-	let regexp = new RegExp(`^${userInput}`, 'gi');
-	clearOutput(output);
-	if (userInput.length > 0){
-		outputRegexHits(names.filter((name) => name.search(regexp) !== -1), output);
-		outputRegexHits(IDs.filter((id) => id.search(regexp) !== -1), output);
-		// for (i in names) {
-		// 	if (names[i].search(regexp) !== -1){
-		// 		console.log("Navn: " + names[i]);
-		// 		//outputRegexHit(names[i], output);
-		// 	}
-		// }
-	}
-}
-
-function clearOutput(output){
-	if (output === undefined){
-		output = document.querySelector(".search-output");
-	}
-	while (output.firstChild) { // Fjerner alle barna til output
-		output.removeChild(output.firstChild);
-	}
-}
-
-function outputRegexHits(hits, output){
-	hits.forEach((hit) => {
-		const li = document.createElement("li");
-		let kommunenr, kommunenavn;
-		(Number(hit)) ? kommunenavn = l.getName(hit): kommunenr = l.getID(hit);  
-		li.className = "search-suggestion";
-
-		li.innerHTML = `${hit} 
-						<span class="search-span">
-						${(kommunenavn) ? kommunenavn : kommunenr}
-						</span>
-							   <span class="search-span">Innbyggertall: 32421</span>`;
-		
-		//li.addEventListener("click", (event) => )
-		output.appendChild(li);
-	});
-}
 
 
 
